178cc93fdc6db1af77b1af51adc66ed1ae4fb9a6
Francisco Abarzúa <git.id@frabarz.cl>
2016-01-27 04:08:33 -0300

    Primer demo funcional con el sistema de módulos

--- example/index.js
+++ example/index.js
@@ -1,39 +1,44 @@
 /* global THREE */
 
-import Promise from 'promise-polyfill';
-
 import Trial from '../src/Trial.js';
 import Courtroom from '../src/Courtroom.js';
-
 import Animation from '../src/Animation.js';
 
-import discusion from './demo.debate.js';
+import Coordenada from '../src/Coordenada.js';
 
-window.juicio = null;
-window.pantalla = null;
+import SceneRenderer from '../src/SceneRenderer.js';
+import HudRenderer from '../src/HudRenderer.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-	var juicio, pantalla,
-		width = window.innerWidth,
-        height = Math.floor(9 / 16 * width);
+	let width = window.innerWidth,
+		height = Math.floor(9 / 16 * width);
 
-    if (height > window.innerHeight) {
-        height = window.innerHeight;
-        width = Math.floor(16 / 9 * height);
-    }
+	if (height > window.innerHeight) {
+		height = window.innerHeight;
+		width = Math.floor(16 / 9 * height);
+	}
 
-	juicio = new Trial(width, height);
-	juicio.scene.add(new Courtroom);
+	let juicio = new Trial(width, height);
 
-    [
-        { id: "naegi",      name: "Makoto Naegi" },
+	let scene = new SceneRenderer(width, height);
+	scene.createCamera();
+	scene.addElement(new Courtroom);
+	juicio.setupRenderer('scene', scene)
+	document.querySelector('body').appendChild(scene.canvas);
+
+	let hud = new HudRenderer(width, height);
+	juicio.setupRenderer('hud', hud);
+	document.querySelector('body').appendChild(hud.canvas);
+
+	[
+		{ id: "naegi",      name: "Makoto Naegi" },
 		{ id: "yamada",     name: "Hifumi Yamada" },
-        { id: "fukawa",     name: "Touko Fukawa" },
+		{ id: "fukawa",     name: "Touko Fukawa" },
 		{ id: "kuwata",     name: "Leon Kuwata" },
 		{ id: "celes",      name: "Celestia Ludenberg" },
 		{ id: "togami",     name: "Byakuya Togami" },
 		{ id: "fujisaki",   name: "Chihiro Fujisaki" },
-		{ id: "hagakure",   name: "Yasuhiro Hagakure" },
+		{ id: "hagakure",   name: "Yasuhiro Hagakure", sprite: 0 },
 		null,
 		{ id: "asahina",    name: "Aoi Asahina" },
 		{ id: "oowada",     name: "Mondo Oowada" },
@@ -42,103 +47,18 @@ document.addEventListener('DOMContentLoaded', function() {
 		{ id: "enoshima",   name: "Junko Enoshima" },
 		{ id: "ishimaru",   name: "Kiyotaka Ishimaru" },
 		{ id: "maizono",    name: "Sayaka Maizono" }
-    ].forEach(juicio.setCharacter, juicio);
+	].forEach(juicio.setCharacter, juicio);
 
-    juicio
-        .appendCanvasTo('body')
-        .setupHUD('body');
-    
-    function stop() {
-        toggle = false;
+	juicio.putTheFuckingBear();
+	juicio.setNarrator('Narrator');
 
-		juicio.animation.stop();
-        document.getElementById('boton').textContent = 'Reiniciar';
+	juicio.changeStage('discussion');
 
-		pantalla = juicio.changeStage('discussion');
-		pantalla.setDialogue(juicio.characters.naegi, 'This is a very early preview test for the dangan-engine project. Press the button in the corner to start.');
-        
-		// juicio.animation.tutorial();
-    }
+	juicio.stage.screen.setDialogue(juicio.characters.naegi, 'This is a very early preview test for the dangan-engine project. Press the button in the corner to start.', true);
 
-    var toggle;
-    document.getElementById('boton').addEventListener('click', function () {
-        toggle = !toggle;
+	juicio.stage.justKeepRendering();
 
-        if (toggle) {
-            this.textContent = 'Detener';
-			juicio.animation
-                .tutorialToNSD()
-                .iniciarNSD([
-                    'Tsumiki’s Examination Results',
-                    'Nidai’s Testimony',
-                    'Mioda’s Testimony',
-                    'Nanami’s Testimony',
-                    'Souda’s Testimony'
-                ])
-                .loadScript(discusion)
-                .delay(2000).promise
-                .then(stop, function (e) {
-                    stop();
-                    console.error(e.stack);
-                });
-        } else {
-            stop();
-        }
-    }, false);
-
-    stop();
-
-	window.juicio = juicio;
-	window.pantalla = pantalla;
-
-	ejecutarConversacionRandom();
+	document.querySelector('#boton').addEventListener('click', function () {
+		juicio.loadScript("/example/demo_001_discussion.json");
+	}, false);
 }, false);
-
-function ejecutarConversacionRandom() {
-	var juicio = window.juicio;
-
-	juicio.mainCamera.up.z = 5;
-	setInterval(rotateDialogue, 10000);
-	rotateDialogue();
-
-	function rotateDialogue() {
-		var transition,
-			inicio,
-			dir_x, dir_y,
-			char = juicio.characters[Object.keys(juicio.characters)[Math.floor(Math.random() * 15)]],
-			counter = char.card.counter;
-
-		screen.setDialogue(char, 'I actually looked at the crime scene for more than 2 seconds, something you should try to do sometimes...');
-
-		juicio.mainCamera.up.x = (Math.random() - 0.5) / 2;
-		juicio.mainCamera.up.normalize();
-
-		dir_x = 0.5 * (Math.random() - 0.5);
-		dir_y = 10 * (Math.random() - 0.5);
-
-		transition = function(t)
-		{
-			var delta = (t - inicio) / 10000,
-				ang = 2 * Math.PI * (counter + dir_x * delta) / 16;
-
-			juicio.mainCamera.position.set(
-				(9 + dir_y * delta) * Math.cos(ang),
-				(9 + dir_y * delta) * Math.sin(ang),
-				16
-			);
-
-			juicio.mainCamera.lookAt({
-				x: 25 * Math.cos(ang),
-				y: 25 * Math.sin(ang),
-				z: 16
-			});
-
-			juicio.render(t);
-			Animation.step(transition);
-		}
-
-		juicio.animation.stop();
-		inicio = window.performance.now() + 5000;
-		Animation.step(transition);
-	}
-}
